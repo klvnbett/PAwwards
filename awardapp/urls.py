cf17dfd44a22db5194c56a6396a43454d2906019
BettK <kelvin.bett@student.moringaschool.com>
2021-09-18 20:19:28 +0300

    added app urls

---
+++ awardapp/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+from  django.conf.urls import url
+from django .conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
+
+
+urlpatterns =[
+    
+]
