1fcfa9571a7b6c7932d39a042b58226f6ac40dd3
BettK <kelvin.bett@student.moringaschool.com>
2021-09-19 02:39:54 +0300

    addded the projects model

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+import datetime as dt
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Projects(models.Model):
+    '''
+    this is the class to generate the user's projects
+    '''
+    title=models.CharField(max_length=125)
+    description=models.TextField
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    '''
+    this is a class to generate the user's profile
+    '''
+    
