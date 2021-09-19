6947aab7fc29e5365b5268abe29e1b0ef5fed66c
BettK <kelvin.bett@student.moringaschool.com>
2021-09-19 12:42:13 +0300

    added for search project

--- awardapp/models.py
+++ awardapp/models.py
@@ -5,27 +5,47 @@ from django.dispatch import receiver
 # Create your models here.
 
 
-class Projects(models.Model):
-    '''
-    this is the class to generate the user's projects
-    '''
-    project_name = models.CharField(max_length=300)
-    image = CloudinaryField('images')
-    description = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User,on_delete=models.CASCADE, blank=True)
-    author_photo = models.ForeignKey(Profile,on_delete=models.CASCADE,blank=True, default='1')
-    url = models.URLField()
-    
-    def save_project(self):
-         self.save()
-
 class Profile(models.Model):
-    '''
-    this is a class to generate the user's profile
-    '''
-    profile=models.ImageField(upload_to='profile')
-    bio= models.TextField(blank=True)
-    user_id=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
-    add_info=models.TextField(blank=False,null=True)
+    dpic = models.ImageField(upload_to = 'images/')
+    bio = models.TextField(max_length=1000)
+    info = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return self.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+class Projects(models.Model):
+    title = models.CharField(max_length=500)
+    description=models.TextField(max_length=2000)
+    link=models.URLField()
+    image=models.ImageField(upload_to='images/')
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+
+
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def all_projects(cls):
     
+    all_projects = cls.objects.all()
+        return all_projects
+
+    @classmethod
+    def one_project(cls,id):
+        one_project = cls.objects.filter(id=id)
+        return one_project
+    @classmethod
+    def user_projects(cls,user):
+        user_projects = cls.objects.filter(user = user)
+        return user_projects
+    
+    @classmethod
+    def search_project(cls,search_term):
+        searched_project = cls.objects.filter(title = search_term)
+        return searched_project
