0e5a46561b343f74ad78b236fa602b2d3baf75ed
BettK <kelvin.bett@student.moringaschool.com>
2021-09-19 13:32:57 +0300

    search project view added

--- awardapp/views.py
+++ awardapp/views.py
@@ -20,3 +20,22 @@ def new_project(request):
             project = form.save(commit=False)
             project.user = request.user
             project.save()
+
+            return redirect('index')
+
+    else:
+        form = NewProjectForm()
+    return render(request,'awards/new_project.html',{'form':form})
+
+def search_results(request):
+
+    if 'project' in request.GET and request.GET['project']:
+        search_term = request.GET.get('project')
+        searched_projects = Projects.search_project(search_term)
+        message = f'{search_term}'
+
+        return render(request,'awards/search.html',{'message':message,'project':searched_projects})
+
+    else:
+        message = 'Kindly Enter a search Value'
+        return render(request,'awards/search.html',{'message':message})
