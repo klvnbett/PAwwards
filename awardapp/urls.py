660d5105c577439f9e0c71f68be865bdb82f470d
BettK <kelvin.bett@student.moringaschool.com>
2021-09-19 13:49:53 +0300

    created urls

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -8,6 +8,12 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns =[
     url('',views.index,name='index'),
+    url('profile/',views.profile,name = 'profile'),
+    url(r'^newproject/$',views.new_project,name='newproject'),
+    url(r'^search/',views.search_results,name = 'search_results'),
+    url('comment/<int:id>/',views.comment,name='comment'),
+    url(r'^editprofile/$',views.edit_profile,name='editprofile'),
+    url(r'^singleproject/(\d+)',views.single_project,name='singleproject'),
 
 ]
 if settings.DEBUG:
